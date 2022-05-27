064d39c07ed2d07701d755989f99aa2b35f3a6b2
Gabriel Sakash <gabrielsakash@gmail.com>
2022-05-27 19:57:20 -0400

    Changed the way rounding is done

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tabulate import tabulate
 
 object_ay = -9.81
 
+roundingAmount = 2
+
 def clear():
     if name == 'nt':
         _ = system('cls')
@@ -16,20 +18,26 @@ print("Enter 1 for Viy and Vix and 2 for Vi and Θ.")
 
 match int(input()):
     case 1:
+        clear()
         print("Input object's initial horizontal velocity.")
         object_vix = float(input())
+        clear()
 
         print("Input object's initial vertical velocity.")
         object_viy = float(input())
+        clear()
 
         object_vi = math.sqrt( object_vix**2 + object_viy**2 )
         object_thetaRadians = math.atan(object_viy/object_vix)
     case 2:
+        clear()
         print("Input object's initial velocity.")
         object_vi = float(input())
+        clear()
 
         print("Input object's angle from the horizontal in degrees.")
         object_thetaRadians = float(input()) * math.pi/180
+        clear()
 
         object_vix = object_vi * math.cos(object_thetaRadians)
         object_viy = object_vi * math.sin(object_thetaRadians)
@@ -47,12 +55,12 @@ object_dx = object_vix * object_tx
 object_dy = (object_viy * object_ty) + (0.5*object_ay*(object_ty**2)) 
 
 data = [
-    ["Vi", round(object_vix, 1), round(object_viy, 1)],
-    ["Vf", round(object_vix, 1), round(-object_viy, 1)],
-    ["a", "0", round(object_ay, 1)],
-    ["d", round(object_dx, 1), round(object_dy, 1)],
-    ["t", round(object_tx, 1), round(object_ty, 1)]
+    ["Vi", round(object_vix, roundingAmount), round(object_viy, roundingAmount)],
+    ["Vf", round(object_vix, roundingAmount), round(-object_viy, roundingAmount)],
+    ["a", "0", round(object_ay, roundingAmount)],
+    ["d", round(object_dx, roundingAmount), round(object_dy, roundingAmount)],
+    ["t", round(object_tx, roundingAmount), round(object_ty, roundingAmount)]
 ]
 
 print(tabulate(data, headers=["Variable", "x", "y"]))
-print("Object's angle was:", (object_thetaRadians*180/math.pi))
+print("Object's angle was:", round(object_thetaRadians*180/math.pi, 2))
