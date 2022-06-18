2404260d2b0ea358fd1562ce7f6585d8d5ceb528
Velo <84917738+Velocities@users.noreply.github.com>
2022-06-18 19:42:24 -0400

    Update choices.h

--- choices.h
+++ choices.h
@@ -12,12 +12,12 @@ void finish() {
   double object_dy = (object_viy * object_ty) + (0.5*object_ay*(pow(object_ty, 2)));
 }
 
-void choice_1() {
+void choice_1(float object_vix, float object_viy) {
   object_vi = sqrt(pow(object_vix, 2) + pow(object_viy, 2));
   object_thetaRadians = atan(object_viy/object_vix);
 }
 
-void choice_2() {
+void choice_2(double object_thetaRadians, double object_vi) {
   object_thetaRadians *= pi/180;
   object_vix = object_vi * cos(object_thetaRadians);
   object_viy = object_vi * sin(object_thetaRadians);
