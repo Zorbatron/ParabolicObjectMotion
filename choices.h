b11c05f3b8dfb232c913f7e96705f747c4e663e6
Velo <84917738+Velocities@users.noreply.github.com>
2022-06-18 19:10:07 -0400

    Update choices.h

--- choices.h
+++ choices.h
@@ -3,6 +3,14 @@
 #define pi 3.14159265358
 using namespace std;
 
+void finish() {
+  double object_vf = sqrt(pow(object_vix, 2) + pow((-object_viy), 2));
+  double object_ty = (-object_viy)/(object_ay);
+  double object_tx = 2 * object_ty;
+  double object_dx = object_vix * object_tx;
+  double object_dy = (object_viy * object_ty) + (0.5*object_ay*(pow(object_ty, 2)));
+}
+
 void choice_1() {
   cout << "Input object's initial horizontal velocity.\n";
   cin >> object_vix;
