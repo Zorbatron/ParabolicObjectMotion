1e03ea5ef014fef83cd041427150537fe3a546bc
Velo <84917738+Velocities@users.noreply.github.com>
2022-06-18 19:22:13 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 #include <cmath>
 #include "choices.h"
 #define pi 3.14159265358
+#define object_ax 0.0
+#define object_ay -9.81
 using namespace std;
 
 int main() {
-  float object_ay = -9.81;
-  float object_ax = 0.0;
   float object_vix;
   float object_viy;
   double object_vi;
