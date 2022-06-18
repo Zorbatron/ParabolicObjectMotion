b6623c62f509cad986e7fcf950c9eb72c4e140a3
Velo <84917738+Velocities@users.noreply.github.com>
2022-06-18 19:34:22 -0400

    Update choices.h

--- choices.h
+++ choices.h
@@ -18,10 +18,6 @@ void choice_1() {
 }
 
 void choice_2() {
-  cout << "Input object's initial velocity.\n";
-  cin >> object_vi;
-  cout << "Input object's angle from the horizontal in degrees.\n";
-  cin >> object_thetaRadians;
   object_thetaRadians *= pi/180;
   object_vix = object_vi * cos(object_thetaRadians);
   object_viy = object_vi * sin(object_thetaRadians);
